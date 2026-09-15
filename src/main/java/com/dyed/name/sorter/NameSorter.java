d107a246f7a217b3511825a362f4b86e8752b375
Viraj Wickramasinghe <viraj@Virajs-MacBook-M3-Pro.local>
2026-09-14 22:20:44 -0400

    Adding the basic template

---
+++ src/main/java/com/dyed/name/sorter/NameSorter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dyed.name.sorter;
+
+/**
+ *
+ * @author viraj
+ */
+public class NameSorter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
